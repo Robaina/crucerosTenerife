f8b3bb7b106425e83515cc6939c48bebebb7cab8
Semidán Robaina-Estévez <tintanjarafe@gmail.com>
2020-02-15 13:22:14 +0000

    update python script

--- getShipData.py
+++ getShipData.py
@@ -4,19 +4,14 @@ Web-scrape basic information about ships
 import requests
 from subprocess import call
 
-worDir = 'C:/Users/tinta/OneDrive/Documents/Web_development/crucerosTenerife/'
+workDir = 'C:/Users/tinta/OneDrive/Documents/Web_development/crucerosTenerife/'
 
 # Download new data file
 url = 'http://risp.puertosdetenerife.org/dataset/eff95e11-4baa-4ab8-aeb2-33d80c6395d8/resource/4b31504e-fd63-4eba-a9ef-6663a12d5dd0/download/crucerosprevistos.csv'
 myfile = requests.get(url)
-open('crucerosprevistos_Tenerife.csv', 'wb').write(myfile.content)
+open(workDir + 'crucerosprevistos_Tenerife.csv', 'wb').write(myfile.content)
 
 # Push commit
-call(['git', 'add', 'crucerosprevistos_Tenerife.csv'])
-call(['git', 'commit', '-m', '"Update data"'])
-call(['git', 'push', 'origin', 'master'])
-
-
-# content = BeautifulSoup(open(url), "html.parser")
-# text = urllib.request.urlopen(url).read()
-# print(text)
+call(['git', 'add', 'crucerosprevistos_Tenerife.csv'], cwd=workDir)
+call(['git', 'commit', '-m', '"Update data"'], cwd=workDir)
+call(['git', 'push', 'origin', 'master'], cwd=workDir)
